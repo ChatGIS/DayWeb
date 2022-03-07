338ffed4805a98a016a84802a4c9c112be31fa0b
Dreamice <dreamice13@foxmail.com>
2022-03-07 17:55:56 +0800

    nobr标签替代

--- script/QueryPanel.js
+++ script/QueryPanel.js
@@ -1,16 +1,24 @@
 var QueryPanel = (function(){
     console.log('Hello QueryPanel');
-    var a = 'aa'
-    console.log(a);
+    var _index = '内部属性'
+    console.log(_index);
     
     var QueryPanelFun = function(){
-        console.log('Hello QueryPanel-Fun');
-        
+        console.log('Hello QueryPanelFun');
+        console.log(_index);
     }
 
-    QueryPanelFun.prototype.init = function (){
+    var QueryPanelSelect = function(){
+        console.log('Hello QueryPanelSelect');
+        console.log(_index);
+    }
+
+    QueryPanelFun.prototype.init = function (params){
         console.log('Hello QueryPanel-init');
     }
 
-    return QueryPanelFun;
+    return {
+        QueryPanelFun: QueryPanelFun,
+        QueryPanelSelect: QueryPanelSelect                                                                               
+    }
 })()
