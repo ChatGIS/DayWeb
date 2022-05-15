0b3b2be62a17709243ab5f9d5491ad6ff071328c
Dreamice <dreamice13@foxmail.com>
2022-05-15 22:19:33 +0800

    弹性布局

--- script/ajax/fetchapi.js
+++ script/ajax/fetchapi.js
@@ -1,8 +1,26 @@
 console.log('进入fetch api模块');
 
-let res = fetch("http://localhost:3007/api/dictionary")
-console.log(res);
+// 简单使用
+fetch("http://localhost:3007/api/dictionary")
+    .then(res => res.json())
+    .then(resJson => {
+        // console.log(resJson)
+        // console.log(resJson.meta);
+        // console.log(resJson.data);
+    })
 
-res.then(val => {
-    console.log(val);
-})
+// 
+fetch("http://localhost:3007/api/dictionary")
+    .then(res => {
+        if(res.ok && res.headers.get("Content-Type") === "application/json; charset=utf-8"){
+            return res.json()
+        }
+    })
+    .then(resJson => {
+        console.log(resJson)
+        console.log(resJson.meta);
+        console.log(resJson.data);
+    })
+    .catch(err => {
+        console.log("Fetch Error", err);
+    })
